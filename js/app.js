0e85499f333f4df7f69a8579306575b138f6509e
Alex Weldetsadik <alexweldetsadik@gmail.com>
2016-07-31 10:41:45 -0400

    added get current function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+var getCurrent = function(city) {
+	var request = {
+		id: city,
+		APPID: '8dac38d11acbe3e6ecf035a449582cac',
+	};
+
+	$.ajax({
+		url: '//api.openweathermap.org/data/2.5/weather',
+		data: request,
+		dataType: 'jsonp',
+		type: 'GET',
+	})
+	.done(function(result){
+		console.log(result);
+		var rslt = showResults(result);
+		$('.result').append(rslt);
+	})
+	.fail(function(){
+
+	});
+};
+
 $(function(){
 	$('.city').submit(function(e){
 		e.preventDefault();
@@ -8,3 +30,4 @@ $(function(){
 	});
 	
 });
+
